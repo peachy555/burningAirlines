d8e24e2f017566d92a5820c15e808954f787a13a
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-04-05 17:50:58 +1000

    SummaryDisplay & minor css

--- app/assets/javascripts/views/FlightsListView.js
+++ app/assets/javascripts/views/FlightsListView.js
@@ -12,17 +12,23 @@ App.FlightsListView = Backbone.View.extend({
     var template = _.template($("#flightsListTemplate").html());
     this.$el.html(template(this.collection));
   },
+
   showSeats: function(event) {
+
+    var $summaryDisplay = $("#summary_display");
+    $summaryDisplay.hide();
+
+
     var flightID = $(event.target.parentElement).attr("id");
     var flight = this.collection.get(flightID);
     var plane = App.airplanes.get(flight.get("airplane_id"));
     var planeRow = plane.get("row");
     var planeCol = plane.get("col");
 
-    $("#seatSelection").empty();
+    $("#seat_selection").empty();
     for (var r = 1; r <= planeRow; r++) {
       var newRow = $("<div>").attr( "id" , "row" + r ).addClass("seatRows");
-      $("#seatSelection").append(newRow);
+      $("#seat_selection").append(newRow);
       for (var c = 1; c <= planeCol; c++) {
         var newSeat = $("<div>").attr( "id" , r + "-" + c ).addClass("seats");
         newRow.append(newSeat);
--- app/assets/javascripts/views/SeatSelectionView.js
+++ app/assets/javascripts/views/SeatSelectionView.js
@@ -2,37 +2,81 @@ var App = App || {};
 var selectedSeat = '';
 
 App.SeatSelectionView = Backbone.View.extend({
+
   initialize: function(options){
     this.listenTo(App.reservations, "change sync", this.render);
   },
+
   events: {
     "click .seats": "select",
-    "click #reservationSubmit": "reservationSubmit"
+    "click #reservation_submit": "reservationSubmit"
   },
-  el: "#seatSelection",
+
+  el: "#seat_selection",
+
   render: function() {
     _.each(this.collection.models, function(reservation) {
       seatDivID = "#" + reservation.attributes.seat_row + "-" + reservation.attributes.seat_col;
       $(seatDivID).css( "backgroundColor", "red" );
     });
   },
-  select: function(event) {
-    var selectedSeatID = $(event.target).attr("id");
-    if(selectedSeat.length !== 0) {
-      $("#" + selectedSeat).css( "backgroundColor", "white" );
-    }
-    selectedSeat = selectedSeatID;
 
-    var checkReservation = _.filter(this.collection.models, function(reservation) {
-      var attr = reservation.attributes;
-      return (attr.seat_row === selectedSeatID.split("-")[0]) &&
-             (attr.seat_col === selectedSeatID.split("-")[1]);
-    });
-    if(checkReservation.length === 0) {
+  select: function(event) {
+    if(this.checkSeatAvailable(event)) {
+      if(selectedSeat.length !== 0) {
+        $("#" + selectedSeat).css( "backgroundColor", "white" );
+      }
+      var selectedSeatID = $(event.target).attr("id");
       $("#" + selectedSeatID).css( "backgroundColor", "gray" );
+      selectedSeat = selectedSeatID;
     } else {
       alert("Seat Reserved.");
     }
     // create reservation
-  }
-})
+
+    this.reservationSummary();
+  },
+
+  checkSeatAvailable: function(event) {
+    var checkReservation = _.filter(this.collection.models, function(reservation) {
+      var attr = reservation.attributes;
+      return (attr.seat_row === $(event.target).attr("id").split("-")[0]) &&
+             (attr.seat_col === $(event.target).attr("id").split("-")[1]);
+    });
+    return checkReservation.length === 0 ? true : false // Available(true), Reserved(false)
+  }, //checkSeatAvailable
+
+  reservationSummary: function() {
+
+    var $summaryDisplay = $("#summary_display");
+    $summaryDisplay.show();
+
+    var flightID = this.collection.models[0].attributes.flight_id;
+    var flightAttr = App.flights.get(flightID).attributes;
+    var planeID = flightAttr.airplane_id;
+    var planeAttr = App.airplanes.get(planeID).attributes;
+    var $reservationSummary = $("#reservation_summary");
+    $reservationSummary.empty();
+    var $flightName = $("<div>").attr("id", "summary_flight_number")
+                                .html("Flight Number: " + flightAttr.flight_number);
+
+    var $flightDate = $("<div>").attr("id", "summary_flight_date")
+                                .html("Date: " + flightAttr.flight_date);
+
+    var $flightDirection = $("<div>").attr("id", "summary_flight_direction")
+                      .html("Direction: " + flightAttr.departure + " -> " + flightAttr.destination);
+
+    var $planeName = $("<div>").attr("id", "summary_plane_name")
+                                .html("Plane Model: " + planeAttr.name);
+
+    var $selectedSeat = $("<div>").attr("id", "summary_selected_seat")
+              .html("Row: " + selectedSeat.split("-")[0] + " Col: " + selectedSeat.split("-")[1]);
+
+    $reservationSummary.append($flightName)
+                       .append($flightDate)
+                       .append($flightDirection)
+                       .append($planeName)
+                       .append($selectedSeat);
+  } //reservationSubmit
+
+}) //App.SeatSelectionView
