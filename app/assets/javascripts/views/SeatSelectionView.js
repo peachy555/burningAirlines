4188ed27fb9879c5fe0c84049823b5e4fb85811f
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-04-05 16:09:00 +1000

    Show reserved seats in red

--- app/assets/javascripts/views/SeatSelectionView.js
+++ app/assets/javascripts/views/SeatSelectionView.js
@@ -2,7 +2,6 @@ var App = App || {};
 
 App.SeatSelectionView = Backbone.View.extend({
   initialize: function(options){
-    debugger
     this.listenTo(App.reservations, "change sync", this.render);
   },
   events: {
@@ -11,8 +10,11 @@ App.SeatSelectionView = Backbone.View.extend({
   },
   el: "#seatSelection",
   render: function() {
-    // console.log("rendering seatSelection");
-    // this.$el.html($("#seatSelectionTemplate").html());
+    console.log("rendering seatSelection");
+    _.each(this.collection.models, function(reservation) {
+      seatDivID = "#" + reservation.attributes.seat_row + "-" + reservation.attributes.seat_col;
+      $(seatDivID).css( "backgroundColor", "red" );
+    })
   },
   select: function() {
     debugger
